:sparkles: Start Q6a Softmax Regression and clean up files Note: Q6a running out of memory after 1st k-fold iteration (TODO: fix)

cse251b_PA1_starter/data.py
@@ -36,7 +36,7 @@ def z_score_normalize(X, u = None, sd = None):
     Parameters
     ----------
     X : np.array
-        The data to min-max normalize
+        The data to z-score normalize
 
     Returns
     -------
@@ -47,7 +47,7 @@ def z_score_normalize(X, u = None, sd = None):
     mu = np.mean(X, axis=0) # calculate mean for each feature col
     sigma = np.std(X, axis=0) # calculate stddev for each feature col
 
-    X_norm = (x - mu) / sigma # normalize each feature col
+    X_norm = (X - mu) / sigma # normalize each feature col
     return X_norm
 
 def min_max_normalize(X, _min = None, _max = None):
@@ -109,7 +109,7 @@ def onehot_decode(y):
     -------
         1d array (length n) of targets (k)
     """
-    indices = np.argmax(y, axis=0)
+    indices = np.argmax(y, axis=1)
     return indices
 
 def shuffle(dataset):
@@ -174,8 +174,8 @@ def generate_k_fold_set(dataset, k=10):
     # Be sure to modify to include train/val/test and need to split train/val/test mutually exclusive(k-2/ 1/ 1)
     Data, labels = dataset
     
-    order = np.random.permutation(len(Data)) # suffle
-    fold_width = len(Data)//10 # division with floor
+    order = np.random.permutation(len(Data)) # shuffle
+    fold_width = len(Data) // k # division with floor
     l_idx, r_idx = 0, fold_width # one fold data length
     # Training set : K-2, Validation set : 1, Test set : 1 first two sets are for validation, test, rest of them are for training
 
@@ -211,6 +211,7 @@ def generate_no_fold_set(dataset, k = 1):
     order = np.random.permutation(len(Data)) # suffle
     fold_width = len(Data)//10 # division with floor
     l_idx, r_idx = 0, fold_width # one fold data length
+    print("FOLDWIDTH:", fold_width)
     # Training set : K-2, Validation set : 1, Test set : 1 first two sets are for validation, test, rest of them are for training
     Traindata = []
     TrainLabel = []

cse251b_PA1_starter/pca.py
@@ -14,7 +14,7 @@ class PCA:
     For the visualization of the principal components, use the internal 
     parameters that are set in `fit`.
     """
-    def __init__(self, x: np.ndarray, num_components):
+    def __init__(self, num_components):
         """
         Setup the PCA object. 
 
@@ -26,7 +26,6 @@ class PCA:
         ----------    
         """
         self.num_components = num_components
-        self.x = x
 
     
     def fit(self, X):
@@ -38,9 +37,37 @@ class PCA:
         X : np.array
             Training data to fit internal parameters.
         """
+        #1. subtract the mean image from every image.
+        self.mean_img = np.average(X, axis=0)
+        msd = X - self.mean_img  # A = M x d ( M : number of Image, d : dimension of the Image(number of pixel)) = 2785 x  1024
+        #2. construct co-variance matrix. #outer product of the two column vector d -> covar = A * A.T
+        # Use Turk Pentland Trick (A^T * A)
+        cov_matrix = np.dot(msd.T, msd) # 1024 x 1024 = d x d ( d x M * M x d)
+        # cov_matrix = np.cov(msd.T)
+        #3. Find eigenvalue, eigen vector
+        eigen_values, eigen_vectors = np.linalg.eigh(cov_matrix)
+        # Get eigenvectors of A
+        # u_i = A * v_i
+        eigen_vectors = np.dot(msd, eigen_vectors)
 
-        pass
+        #4. Sort the eigen vectors with the largest eigenvalue -> first principal component
+        # -1) sorting eigen value and eigenvector
+        idx = eigen_values.argsort()[::-1]
+        eigen_values = eigen_values[idx]
+        eigen_vectors = eigen_vectors[:,idx]
 
+        # -2) the Avi’s are actually the eigenvectors of the original huge matrix C
+        # # map vector from original
+        eigen_vectors = (np.matmul(msd.T, eigen_vectors)).T  # M x d
+        # -3) projection
+        #self.normalized_eig_vecs = eigen_vectors / np.linalg.norm(eigen_vectors, 2, axis=0)
+        norm = np.sqrt(np.sum((eigen_vectors)**2, axis=-1)).reshape(-1,1)  # M
+        self.normalized_eig_vecs = eigen_vectors / norm # M x d
+
+        self.principal_eigen_vectors = self.normalized_eig_vecs[:self.num_components].T
+
+        #5. divide by the standard deviation of the projections, which is the square root of the eigenvalue
+        self.principal_sqrt_eigen_values = np.sqrt(eigen_values[:self.num_components])
 
     def transform(self, X):
         """
@@ -58,13 +85,16 @@ class PCA:
         -------
             Transformed dataset with lower dimensionality
         """
-
-        pass
+        X = X - self.mean_img
+        # Project
+        projected = np.matmul(X, self.principal_eigen_vectors) / self.principal_sqrt_eigen_values
+        
+        return projected
 
 
     def fit_transform(self, X):
         self.fit(X)
-        pass 
+        return self.transform(X)
 
     def plot_PC(self):
         '''
@@ -84,12 +114,12 @@ class PCA:
         axs[1, 1].imshow(self.principal_eigen_vectors.T[3].real.reshape((32, 32)))
         plt.show()       
 
-    def PCA_Emmet(self) :
+    def PCA_Emmet(self, X) :
         #1. subtract the mean image from every image.
-        self.mean_img = np.average(self.x, axis=0)
-        msd = self.x - self.mean_img  # A = M x d ( M : number of Image, d : dimension of the Image(number of pixel)) = 2785 x  1024
+        self.mean_img = np.average(X, axis=0)
+        msd = X - self.mean_img  # A = M x d ( M : number of Image, d : dimension of the Image(number of pixel)) = 2785 x  1024
         #2. construct co-variance matrix. #outer product of the two column vector d -> covar = A * A.T
-        cov_matrix = np.matmul(msd, msd.T) # 2785 x 2785 = M x M ( M x d * d x M)
+        cov_matrix = np.dot(msd, msd.T) # 2785 x 2785 = M x M ( M x d * d x M)
         #3. Find eigenvalue, eigen vector
         eigen_values, eigen_vectors = np.linalg.eigh(cov_matrix)
         #4. Sort the eigen vectors with the largest eigenvalue -> first principal component