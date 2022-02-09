45d85f7cc6f73df05bb81335ff292bdd6bc36fc5
shanqingwang <shw019@ucsd.edu>
2022-02-09 11:07:04 -0800

    imlp eval metrics

--- PA3_starter/utils.py
+++ PA3_starter/utils.py
@@ -10,14 +10,27 @@ def iou(pred, target, n_classes = 10):
   for cls in range(n_classes-1):  # last class is ignored
     pred_inds = pred == cls
     target_inds = target == cls
-    intersection = __ #complete this
-    union = __ #complete this
+    intersection = sum(np.logical_and(pred_inds,target_inds)) #DONE: complete this, number of agreements, TP
+    union = sum(np.logical_or(pred_inds,target_inds)) #DONE: complete this, total TP + FP + FN
     if union == 0:
       ious.append(float('nan'))  # If there is no ground truth, do not include in evaluation
     else:
-      __ #complete this
+      ious.append(float(intersection/union)) 
 
   return np.array(ious)
 
 def pixel_acc(pred, target):
-    #TODO complete this function, make sure you don't calculate the accuracy for undefined class ("9")
+    #DONE: TODO complete this function, make sure you don't calculate the accuracy for undefined class ("9")
+    total=0
+    correct=0
+    
+    for i in range(len(target)): # count correct prediction and total for non-undefined classes
+      if target[i] == 9: 
+        continue # ignore undefined class
+      
+      if target[i] == pred[i]:
+        correct+=1
+
+      total+=1
+    
+    return total/correct
