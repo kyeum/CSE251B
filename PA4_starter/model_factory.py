4ef3a6a0236eba18bbd3b3f0ee8ff22c9e493e37
Victor Zhu <victorvctr.zh@gmail.com>
2022-02-23 22:15:32 -0800

    :construction: Add baseline LSTM model (encoder + decoder)

--- PA4_starter/model_factory.py
+++ PA4_starter/model_factory.py
@@ -5,16 +5,16 @@
 ################################################################################
 from torchvision import models
 import torch.nn as nn
-
+from models import LSTM, LSTMEncoder, LSTMDecoder
 
 # Build and return the model here based on the configuration.
 def get_model(config_data, vocab):
-    hidden_size = config_data['model']['hidden_size']
-    embedding_size = config_data['model']['embedding_size']
     model_type = config_data['model']['model_type']
     
-    if model_type is 'LSTM':
-        return get_model_LSTM(config, vocab)
+    if model_type == 'LSTM':
+        model = get_model_LSTM(config, vocab)
+        if model:
+            return get_model_LSTM(config, vocab)
         
     # You may add more parameters if you want
 
@@ -27,51 +27,13 @@ def get_model_LSTM(config_data, vocab, DEBUG=True):
     num_layers = 2
     vocab_size = len(vocab) # TODO
     
-    if model_type is not 'LSTM':
+    if model_type != 'LSTM':
         return False
+    
+    encoder = LSTMEncoder(image_embedding_size=embedding_size)
+    decoder = LSTMDecoder(vocab_size=vocab_size, hidden_size=hidden_size, word_embedding_size=embedding_size, num_layers=num_layers):
+    model = LSTM(encoder, decoder)
         
-    ### Encoder
-    encoder = models.resnet50(pretrained=True)
-    
-    ## Feature Extraction
-    # Freeze all layers of pretrained encoder
-    if feature_extracting:
-        for param in model.parameters():
-            param.requires_grad = False
-            
-    fc_in_features = encoder.fc.in_features
-    
-    if DEBUG:
-        print("fc_in_features:", fc_in_features)
-        print("embedding_size:", embedding_size)
-
-    # Replace last layer with weight layer to embedding dimension
-    encoder.fc = nn.Linear(fc_in_features, embedding_size)
-    
-    # training
-    seq = [image_emb, sen_embed]
-    # inf
-    seq = [image_emb]
-    
-    ### Decoder
-    # input shape = batch_size x sequence_length x input_size
-    decoder = nn.LSTM(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers, proj_size=hidden_size, batch_first=True)
-    
-    decoder2vocab = nn.Linear(hidden_size, vocab_size)
-    
-# batch x seq_len (2) x feature_size
-
-    # Image to image embed
-    # Word to word embedding
-    model = nn.Sequential([
-        encoder,
-        decoder,
-        decoder2vocab,
-        nn.Softmax(dim=2)
-    ])
-    # output shape = batch_size x sequence_length x vocab_size
-        
-#     raise NotImplementedError("Model Factory LSTM Not Implemented")
     return model
 
         
---
+++ PA4_starter/models.py
@@ -0,0 +1,130 @@
+from torchvision import models
+import torch.nn as nn
+
+class LSTM(nn.module):
+    def __init__(self, encoder, decoder):
+        self.encoder = encoder
+        self.decoder = decoder
+    
+    def forward(self, images, captions):
+        inference = False
+        if captions is None:
+            inference = True
+        
+        encoded_images = self.encoder(images)
+        
+        out = self.decoder(encoded_images, captions)
+        
+        return out
+        
+class LSTMEncoder(nn.module):
+    def __init__(self, image_embedding_size=300):
+        super(LSTMEncoder, self).__init__()
+        ### Encoder
+        self.encoder = models.resnet50(pretrained=True)
+
+        ## Feature Extraction
+        # Freeze all layers of pretrained encoder.
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+        fc_in_features = self.encoder.fc.in_features
+
+        # Replace last layer with weight layer to embedding dimension.
+        # Don't need to unfreeze as new linear layer has grads enabled.
+        self.encoder.fc = nn.Linear(fc_in_features, image_embedding_size)
+        
+    def forward(self, images):
+        """
+        Input = Batch_size x Image_height x Image_width
+        Output = Batch_size x Image_embedding_size
+        """
+        return self.encoder(images)
+    
+class LSTMDecoder(nn.module):
+    def __init__(self, vocab_size=-1, hidden_size=512, word_embedding_size=300, num_layers=2):
+        # input: (N, L, H_in) = batch_size x seq_len x input_size
+        # output: (N, L, D * H_out) = batch_size x seq_len, proj_size) 
+        #     [here proj_size=hidden_size]
+        self.decoder = nn.LSTM(input_size=word_embedding_size, hidden_size=hidden_size, num_layers=num_layers, proj_size=hidden_size, batch_first=True)
+        
+        ### Given a vocab word index, returns the word embedding
+        # Input: (*) indices of embedding
+        # Output: (*, H) where * is input shape and H = embedding_dim
+        self.vocab2wordEmbed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=word_embedding_size)
+        
+        # Converts from LSTM output to vocab size
+        self.decoder2vocab = nn.Linear(hidden_size, vocab_size)
+        
+        # Softmax
+        self.softmax = nn.Softmax(dim=2)
+        
+        # Constants
+        self.EOS_TOK = 0;
+        self.DETERMINISTIC = 0
+        self.STOCHASTIC = 1
+    
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+    
+    # encoded_caption is in form of vocab word indices
+    def forward(self, encoded_image, captions):
+        # TODO: add case when captions is empty
+        caption_embeddings = self.vocab2wordEmbed(captions)
+        
+        # TODO: Initialize our LSTM with the image encoder output to bias our prediction.
+        
+        # TODO: Weight initialization
+        
+        
+        # Get output and hidden states
+        out, hidden = self.decoder(caption_embeddings, encoded_image)
+        
+        out = self.decoder2vocab(out)
+        
+        # Get probabilities of each word
+        out = self.softmax(out)
+        return out # shape: batch_size x seq_len x vocab_size
+        
+    # Inference
+    def generate_caption(self, encoded_image, sampling_mode=1, max_seq_len=12, end_at_eos=True):
+        """
+        Sampling_mode = 0 deterministic
+                      = 1 stochastic
+        """
+        word_seq = [] # indices of words
+        
+        for i in range(max_seq_len):
+            out = self.forward(encoded_image, word_seq)
+            # Get word indice based on sampling_mode
+            if sampling_mode == self.DETERMINISTIC:
+                wordIndice = out[2].argmax()
+                word_seq.append(wordIndice)
+            else:
+                gen_word_index = torch.multinomial(input=out, num_samples=1, replacement=True)
+                word_seq.append(gen_word_index)
+                
+            if wordIndice == self.EOS_TOK:
+                return word_seq
+            
+        return word_seq
+        
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
